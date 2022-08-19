7806d3d3c39fb46f0644c7b0ee43f277c9458084
Alassani ABODJI <abodjialassani@gmail.com>
2022-08-19 19:11:52 +0000

    Add Quack and Fly interfaces and implementations

---
+++ behavioral/strategy/src/main/java/com/waadsoft/strategy/FlyBehavior.java
@@ -0,0 +1,15 @@
+package com.waadsoft.strategy;
+
+/**
+ *
+ * @author Alassani ABODJI <abodjialassani[at]gmail.com>
+ */
+public interface FlyBehavior {
+    void fly();
+}
+
+/*=============================================================================
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ =============================================================================*/
---
+++ behavioral/strategy/src/main/java/com/waadsoft/strategy/FlyNoWay.java
@@ -0,0 +1,20 @@
+package com.waadsoft.strategy;
+
+/**
+ *
+ * @author Alassani ABODJI <abodjialassani[at]gmail.com>
+ */
+public class FlyNoWay implements FlyBehavior {
+
+    @Override
+    public void fly() {
+        //
+    }
+
+}
+
+/*=============================================================================
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ =============================================================================*/
