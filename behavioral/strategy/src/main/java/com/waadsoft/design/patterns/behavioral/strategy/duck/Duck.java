f70b1cce21e010c678a37c81abc626406ca2095c
Alassani ABODJI <abodjialassani@gmail.com>
2022-08-21 07:22:10 +0000

    Strategy design pattern
    Implementing strategy design pattern

---
+++ behavioral/strategy/src/main/java/com/waadsoft/design/patterns/behavioral/strategy/duck/Duck.java
@@ -0,0 +1,54 @@
+package com.waadsoft.design.patterns.behavioral.strategy.duck;
+
+/**
+ *
+ * @author Alassani ABODJI <abodjialassani[at]gmail.com>
+ */
+public abstract class Duck {
+
+    protected Duck(FlyBehavior flyBehavior, QuackBehavior quackBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+
+    protected FlyBehavior flyBehavior;
+    protected QuackBehavior quackBehavior;
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+}
+
+/*=============================================================================
+ * Copyright 2022 Waad Soft<https://www.waadsoft.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ =============================================================================*/
---
+++ behavioral/strategy/src/main/java/com/waadsoft/design/patterns/behavioral/strategy/duck/MiniDuckSimulator.java
@@ -0,0 +1,37 @@
+package com.waadsoft.design.patterns.behavioral.strategy.duck;
+
+/**
+ *
+ * @author Alassani ABODJI <abodjialassani[at]gmail.com>
+ */
+public class MiniDuckSimulator {
+
+    public static void main(String[] args) {
+        Duck mallardDuck = new MallardDuck();
+        mallardDuck.performQuack();
+        mallardDuck.performFly();
+        
+        System.out.println("-------------------------");
+         
+        Duck modelDuck = new ModelDuck();
+        modelDuck.performFly();
+        modelDuck.setFlyBehavior(new FlyRocketPowered());
+        modelDuck.performFly();
+    }
+}
+
+/*=============================================================================
+ * Copyright 2022 Waad Soft<https://www.waadsoft.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ =============================================================================*/
